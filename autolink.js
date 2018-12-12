4d7fdccaf2caa28674370d4d6fe70d4c8eadaab5
Ellen Marie Dash <me@duckie.co>
2018-12-12 15:58:46 -0500

    just pretend i never did that

--- autolink.js
+++
@@ -1,6 +0,0 @@
-// This is bad and I feel bad.
-// Change <http://...> and <https://...> into proper links.
-window.onload = function () {
-  var text = document.body.innerHTML;
-  document.body.innerHTML = text.replace(/<(https?:\/\/.*)>/, '<a href="$1">$1</a>')
-}
