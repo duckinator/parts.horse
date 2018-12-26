7aff8a2cddc7bc4661c27dddf4caaee91d70f870
Ellen Marie Dash <me@duckie.co>
2018-12-26 18:10:41 -0500

    Allow links without trailing slashes to work.

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',
         'server.socket_port': int(os.environ.get('PORT', 5000)),
+        'tools.trailing_slash.on': False,
     })
 
     cherrypy.engine.start()
