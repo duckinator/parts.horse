4f8dada89224379e7ad7550129caf826f00aa357
Ellen Marie Dash <me@duckie.co>
2018-12-29 23:52:03 -0500

    try enabling gzip

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
         'server.socket_host': '0.0.0.0',
         'server.socket_port': int(os.environ.get('PORT', 5000)),
         'tools.trailing_slash.on': False,
+        'tools.gzip.on': True,
     })
 
     cherrypy.engine.start()
