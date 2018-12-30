fbfd00d8f016ca365b0ea4873fe5a0afd672d3db
Ellen Marie Dash <me@duckie.co>
2018-12-30 00:39:26 -0500

    actually use config files for things.

--- app.py
+++ app.py
@@ -6,32 +6,14 @@ from web.datasheets import Datasheets
 from web.directory  import Directory
 from web.search     import Search
 
-from pathlib import Path
-import os
-
 if __name__ == '__main__':
-    site_dir = str(Path(__file__).resolve().parent)
+    cherrypy.tree.mount(Home(),       '/',           'config/home.conf')
+    cherrypy.tree.mount(Datasheets(), '/datasheets', 'config/app.conf')
+    cherrypy.tree.mount(Datasheets(), '/ds',         'config/app.conf')
+    cherrypy.tree.mount(Directory(),  '/parts',      'config/app.conf')
+    cherrypy.tree.mount(Search(),     '/search',     'config/app.conf')
 
-    home_config = {
-            '/': {
-                'tools.staticdir.root': site_dir,
-                'tools.staticdir.on': True,
-                'tools.staticdir.dir': 'public',
-                }
-            }
-
-    cherrypy.tree.mount(Home(),       '/',       home_config)
-    cherrypy.tree.mount(Datasheets(), '/datasheets')
-    cherrypy.tree.mount(Datasheets(), '/ds')
-    cherrypy.tree.mount(Directory(),  '/parts')
-    cherrypy.tree.mount(Search(),     '/search')
-
-    cherrypy.config.update({
-        'server.socket_host': '0.0.0.0',
-        'server.socket_port': int(os.environ.get('PORT', 5000)),
-        'tools.trailing_slash.on': False,
-        'tools.gzip.on': True,
-    })
+    cherrypy.config.update('config/server.conf')
 
     cherrypy.engine.start()
     cherrypy.engine.block()
