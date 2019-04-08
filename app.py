fc8d6ffc7105d1dcf3428006cb33a39a19aed862
Ellen Marie Dash <me@duckie.co>
2019-04-08 09:20:49 -0400

    Move the majority of app.py into web/__init__.py in preparation for wsgi setup.

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python3
 
-import cherrypy
-from web.api            import Api
-from web.home           import Home
-from web.datasheets     import Datasheets
-from web.diagnostics    import Diagnostics
-from web.directory      import Directory
-from web.search         import Search
+import web
 
 if __name__ == '__main__':
-    cherrypy.tree.mount(Home(),         '/',            'config/home.conf')
-    cherrypy.tree.mount(Api(),          '/api',         'config/app.conf')
-    cherrypy.tree.mount(Datasheets(),   '/datasheets',  'config/app.conf')
-    cherrypy.tree.mount(Datasheets(),   '/ds',          'config/app.conf')
-    cherrypy.tree.mount(Diagnostics(),  '/diagnostics', 'config/app.conf')
-    cherrypy.tree.mount(Directory(),    '/parts',       'config/app.conf')
-    cherrypy.tree.mount(Search(),       '/search',      'config/app.conf')
-
-    cherrypy.config.update('config/server.conf')
-
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+    web.start()
+    web.block()
--- web/__init__.py
+++ web/__init__.py
@@ -1,1 +1,25 @@
+import cherrypy
+from .api           import Api
+from .home          import Home
+from .datasheets    import Datasheets
+from .diagnostics   import Diagnostics
+from .directory     import Directory
+from .search        import Search
 
+def prepare():
+    cherrypy.tree.mount(Home(),         '/',            'config/home.conf')
+    cherrypy.tree.mount(Api(),          '/api',         'config/app.conf')
+    cherrypy.tree.mount(Datasheets(),   '/datasheets',  'config/app.conf')
+    cherrypy.tree.mount(Datasheets(),   '/ds',          'config/app.conf')
+    cherrypy.tree.mount(Diagnostics(),  '/diagnostics', 'config/app.conf')
+    cherrypy.tree.mount(Directory(),    '/parts',       'config/app.conf')
+    cherrypy.tree.mount(Search(),       '/search',      'config/app.conf')
+
+    cherrypy.config.update('config/server.conf')
+
+def start():
+    prepare()
+    cherrypy.engine.start()
+
+def block():
+    cherrypy.engine.block()
