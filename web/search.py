3a77a1c6127d15e7f5906dfabf7adad9c3811108
Ellen Marie Dash <me@duckie.co>
2020-03-04 21:06:16 -0500

    Merge web/base.py into web/search.py, since it's only used once.

--- web/search.py
+++ web/search.py
@@ -1,8 +1,39 @@
+import json
 import cherrypy
 from lib.model.part import Part
-from .base import PartsHorseBase
+from lib.render import PHRender
 
-class Search(PartsHorseBase):
+
+class Search:
+    def __init__(self):
+        self.phrender = PHRender()
+
+        self.env = self.phrender.env
+        self.template = self.phrender.get_class_template(self.__class__)
+
+    def render(self, page=None):
+        """Render the template associated with the page.
+
+        If the client requests JSON, then this returns a JSON dump of the
+        """
+
+        if page is None:
+            page = {}
+
+        headers = cherrypy.response.headers
+
+        if self._is_json_response():
+            headers['Content-Type'] = 'application/json; charset=utf-8'
+            return json.dumps(page, indent=2, sort_keys=True).encode('utf-8')
+
+        return self.template.render(page=page)
+
+    @staticmethod
+    def _is_json_response():
+        headers = cherrypy.response.headers
+        accepted = headers.get('Accept', '').split(',')
+        return 'application/json' in accepted
+
     @staticmethod
     def chunk_relevance(data, chunk):
         chunk = chunk.lower()
@@ -27,7 +58,7 @@ class Search(PartsHorseBase):
     def _search(self, query, min_relevance=1):
         # Split by word, and remove empty strings, None, etc.
         # FIXME: Why the hell was this variable unused?! #pylint: disable=fixme
-        #chunks = filter(lambda x: x, query.split(' '))
+        # chunks = filter(lambda x: x, query.split(' '))
         relevances = map(lambda p: [p, self.relevance(p, query)], Part.names())
         results_f = filter(lambda x: x[1] >= min_relevance, relevances)
         results = map(lambda r: Part.get_dict(r[0], {'relevance': r}), results_f)
