1cf2c08ca2285bf00a9f5843e2fc87ead6c05e15
Ellen Marie Dash <me@duckie.co>
2020-04-30 01:08:12 -0400

    Delete the Elasticsearch index and recreate it with each deploy.

--- bin/index.sh
+++ bin/index.sh
@@ -6,10 +6,16 @@ export PATH="$PATH:$HOME/.local/bin"
 cd $(realpath $(dirname $0))/..
 
 cd config
+
+# Delete the index, if it exists.
+curl -H "Content-Type: application/json" -XDELETE "localhost:9200/parts"
+
+# Create the index, with the current config.
 curl -H "Content-Type: application/json" -XPUT "localhost:9200/parts" --data-binary "@elasticsearch-analyzer-config.json"
 
 cd ../parts
 
+# Populate the index.
 for file in *.json; do
   name=$(echo $file | sed 's/.json$//')
   curl -H "Content-Type: application/json" -XPOST "localhost:9200/parts/_doc/${name}" --data-binary "@${file}"
