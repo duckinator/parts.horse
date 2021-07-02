1bf90f0a9436ce1c946c270fa3a5ea561ffe034c
Ellen Marie Dash <me@duckie.co>
2021-07-02 01:23:09 -0400

    locally disable consider-using-with lint error.

--- bin/test.py
+++ bin/test.py
@@ -134,7 +134,7 @@ class CheckRunner:
 
         for attempt in range(1, self.attempts):
             try:
-                result = get(url).read().decode()
+                result = get(url).read().decode()  # pylint: disable=consider-using-with
             except: #pylint: disable=bare-except
                 exc_type, exc_value, _ = sys.exc_info()
                 if attempt <= self.attempts:
