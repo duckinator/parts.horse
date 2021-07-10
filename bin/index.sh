d6bea53d0026c20e94e9d6c0839658a137adcf28
nicoo <nicoo@mur.at>
2021-07-10 15:50:25 +0200

    bin/index.sh: Honor `$VERBOSE`

--- bin/index.sh
+++ bin/index.sh
@@ -8,20 +8,26 @@ if [ -z "${ELASTICSEARCH}" ]; then
   ELASTICSEARCH="http://localhost:9200"
 fi
 
+curl() {
+  if [ -z "$VERBOSE" ]; then
+    command curl -s -H "Content-Type: application/json" "$@" >/dev/null
+  else
+    command curl -H "Content-Type: application/json" "$@"
+  fi
+}
 
 cd config
 
 # Delete the index, if it exists.
-curl -H "Content-Type: application/json" -XDELETE "${ELASTICSEARCH}/parts"
+curl -XDELETE "${ELASTICSEARCH}/parts"
 
 # Create the index, with the current config.
-curl -H "Content-Type: application/json" -XPUT "${ELASTICSEARCH}/parts" --data-binary "@elasticsearch-analyzer-config.json"
+curl -XPUT "${ELASTICSEARCH}/parts" --data-binary "@elasticsearch-analyzer-config.json"
 
 cd ../parts
 
 # Populate the index.
 for file in *.json; do
   name=$(echo $file | sed 's/.json$//')
-  curl -H "Content-Type: application/json" -XPOST "${ELASTICSEARCH}/parts/_doc/${name}" --data-binary "@${file}"
-  echo
+  curl -XPOST "${ELASTICSEARCH}/parts/_doc/${name}" --data-binary "@${file}"
 done
