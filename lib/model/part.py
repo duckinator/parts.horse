7bb5d16563de2a5333529d35003c2bc23ae05fae
Ellen Marie Dash <me@duckie.co>
2022-03-28 21:40:37 -0400

    Remove now-unnecessary use of Quart.
    Revert "lib.model.Part: Defend against path traversal"
    
    This reverts commit ba5e4ca04b3e01422ed0ee45cf665bb9f6ee300d.
    
    Since Parts Horse is going to be a static site, we don't need Quart,
    and we don't need to defend against this.

--- lib/model/part.py
+++ lib/model/part.py
@@ -1,7 +1,6 @@
 import json
 from functools import lru_cache
 from pathlib import Path
-from quart import safe_join
 
 class Part(object):
     @staticmethod
@@ -28,7 +27,7 @@ class Part(object):
 
     def __init__(self, part_name):
         self.part_name = part_name.replace('/', '-').lower()
-        self.file = safe_join(Path('parts'), self.part_name + '.json')
+        self.file = Path('parts').joinpath(self.part_name + '.json')
 
         try:
             with self.file.open() as f:
